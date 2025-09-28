9cbe4b4d4cc7cf7e5a11ee6a0c18afc23d620bd2
Arjun Malhotra <94740065+ArjunMal1311@users.noreply.github.com>
2025-09-28 10:07:18 +0530

    client, server

---
+++ server/resolvers.ts
@@ -0,0 +1,54 @@
+import { GraphQLError } from "graphql";
+import { getCompany } from "./db/companies";
+import { countJobs, getJob, getJobs, getJobsByCompany } from "./db/jobs";
+
+export const resolvers = {
+    Query: {
+        company: async (_root, { id }) => {
+            const company = await getCompany(id);
+            if (!company) {
+                throw notFoundError('No Company found with id ' + id);
+            }
+            return company;
+        },
+        job: async (_root, { id }) => {
+            const job = await getJob(id);
+            if (!job) {
+                throw notFoundError('No Job found with id ' + id);
+            }
+            return job;
+        },
+        jobs: async (_root, { limit, offset }) => {
+            const items = await getJobs(limit, offset);
+            const totalCount = await countJobs();
+            return { items, totalCount };
+        },
+    },
+
+    Company: {
+        jobs: (company) => getJobsByCompany(company.id),
+    },
+
+    Job: {
+        company: (job, _args, { companyLoader }) => {
+            return companyLoader.load(job.companyId);
+        },
+        date: (job) => toIsoDate(job.createdAt),
+    },
+}
+
+
+function notFoundError(message) {
+    return new GraphQLError(message, {
+        extensions: { code: 'NOT_FOUND' },
+    });
+}
+function unauthorizedError(message) {
+    return new GraphQLError(message, {
+        extensions: { code: 'UNAUTHORIZED' },
+    });
+}
+
+function toIsoDate(value) {
+    return value.slice(0, 'yyyy-mm-dd'.length);
+}
